c2a64a22b26cf8b6b34c5e6fca19abf84ee021e7
netsysmis <netsysmis@gmail.com>
2026-07-23 14:00:00 -0400

    Production static site deployment with all images and content

--- src/pages/rss.xml.ts
+++
@@ -1,4 +0,0 @@
-// The Chocolate Future - Simple static site (no blog/RSS)
-export const GET = async () => {
-  return new Response('<rss xmlns:atom="http://www.w3.org/2005/Atom"><atom:title>The Chocolate Future</atom:title><description>Ghana\'s Cocoa Value Chain Initiative</description></rss>');
-};
